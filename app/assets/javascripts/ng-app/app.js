dcd2e3586615e3f5cc43796e30e57bf83afe26a8
Thanh Nguyen <thanhnguyen.ldt@gmail.com>
2016-04-20 22:04:01 -0500

    Finish home page

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,8 @@
+angular.module('AngularRails', ['ngRoute', 'templates'])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'home.html',
+                controller: 'HomeCtrl'
+            });
+    });
